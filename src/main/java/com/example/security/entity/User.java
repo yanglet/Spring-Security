c921e891fd8264ad400dd986963aaa94391b6462
yanglet <y2yone023@naver.com>
2022-04-05 23:54:53 +0900

    resolve #1, pk 전략 변경

--- src/main/java/com/example/security/entity/User.java
+++ src/main/java/com/example/security/entity/User.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -11,7 +12,7 @@ import javax.persistence.Id;
 @Builder
 @AllArgsConstructor
 public class User extends BaseEntity {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
