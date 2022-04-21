7ca43937fc66ed0cf6650ffff7f5ecf180dc0f1e
yanglet <y2yone023@naver.com>
2022-04-22 08:05:09 +0900

    #6, 무중단 배포를 위한 스크립트 파일, 배포 파일 추가 및 수정

---
+++ scripts/profile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# 쉬고 있는 profile 찾기 : real1 사용 중이면 real2가 쉼 반대면 real1가 쉼
+
+function find_idle_profile() {
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "${http_code}" http://localhost/profile)
+
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400보다 크면 에러 4xx, 5xx
+    then
+      CURRENT_PROFILE=real2
+    else
+      CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
+
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
+
+    echo "${IDLE_PROFILE}"
+}
+
+function find_idle_port() {
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "9091"
+    else
+      echo "9092"
+    fi
+}
