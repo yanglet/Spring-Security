99a019b853d24254a83487c1ed36fc79240edc05
yanglet <y2yone023@naver.com>
2022-04-22 10:39:15 +0900

    resolved #6, 무중단 배포 환경 구축

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
-    IDLE_PORT=${find_idle_port}
+    IDLE_PORT=$(find_idle_port)
 
     echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
