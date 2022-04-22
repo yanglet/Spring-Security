235cb431029cd282bb920f255aaf284d2d788a3d
yanglet <y2yone023@naver.com>
2022-04-22 09:46:00 +0900

    #6, 무중단 배포를 위한 스크립트 파일, 배포 파일 추가 및 수정 9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #1/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step3
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=Spring-Security
 
 echo "> Build 파일복사"
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-       -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.yml \
+       -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml/home/ec2-user/app/application-real-db.yml \
        -Dspring.profiles.active=real \
        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
